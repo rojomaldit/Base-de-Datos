d157bacf2945cecc0c65c891a20ad42b0557c0be
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-08-30 15:24:05 -0300

    Finished EJ13

--- lab1/movies-query.sql
+++ lab1/movies-query.sql
@@ -9,3 +9,10 @@ SELECT title
 SELECT actor_name FROM actor_rol JOIN actors
     WHERE actor_rol.rol_name = '' 
     AND actors.AID = actor_rol.AID;
+
+-- EJ 13
+
+SELECT actor_name, title
+  FROM movies
+    JOIN actor_rol ON movies.MID = actor_rol.MID
+    JOIN actors ON actors.AID = actor_rol.AID;
