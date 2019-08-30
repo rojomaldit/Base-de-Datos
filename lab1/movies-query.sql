5c6a44a0cc0293ec12bc084fe8a756486c554930
rojomaldit <lucaserfurt@hotmail.com>
2019-08-30 15:07:33 -0300

    Changes the name of the movies

--- lab1/movies-query.sql
+++ lab1/movies-query.sql
@@ -7,3 +7,6 @@
 SELECT movies.title FROM movies JOIN actors JOIN actor_rol
 WHERE actors.actor_name = 'Charlie Chaplin';
 
+-- EJ 12
+SELECT actor_name FROM actor_rol JOIN actors
+WHERE actor_rol.rol_name = ''
