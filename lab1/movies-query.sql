baf4e25cd78d0e3bb8414319eac2299c41bdfb15
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-08-30 15:08:18 -0300

    Added EJ11

--- lab1/movies-query.sql
+++ lab1/movies-query.sql
@@ -1,9 +1,20 @@
--- Muestra los datos de cada tabla
--- SELECT * FROM actors
--- SELECT * FROM movies
--- SELECT * FROM actor_rol 
-
 -- EJ 11 
-SELECT movies.title FROM movies JOIN actors JOIN actor_rol
-WHERE actors.actor_name = 'Charlie Chaplin';
 
+SELECT title
+  FROM movies
+    JOIN actor_rol ON movies.MID = actor_rol.MID
+    JOIN actor_rol ON actors.AID = actor_rol.AID
+  WHERE actor_name = "Octa"
+
+-- SELECT column-names
+--   FROM table-name1 JOIN table-name2
+--     ON column-name1 = column-name2
+--  WHERE condition
+
+
+-- SELECT O.OrderNumber, CONVERT(date,O.OrderDate) AS Date, 
+--        P.ProductName, I.Quantity, I.UnitPrice 
+--   FROM [Order] O 
+--   JOIN OrderItem I ON O.Id = I.OrderId 
+--   JOIN Product P ON P.Id = I.ProductId
+-- ORDER BY O.OrderNumber
