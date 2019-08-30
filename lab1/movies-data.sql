cfed2d7ad46a8fd7337f94adf84cfc1a6f3d2ff2
rojomaldit <lucaserfurt@hotmail.com>
2019-08-30 14:41:59 -0300

    nc

--- lab1/movies-data.sql
+++ lab1/movies-data.sql
@@ -5,6 +5,7 @@ delete from actor_rol;
 INSERT INTO actors (AID, actor_name) VALUES (1, 'Uzii');
 INSERT INTO actors (AID, actor_name) VALUES (2, 'Octa');
 INSERT INTO actors (AID, actor_name) VALUES (3, 'Luca');
+INSERT INTO actors (AID, actor_name) VALUES (40, 'Actor que no actua');
 INSERT INTO actors (AID, actor_name) VALUES (4, 'Cardelino :D');
 INSERT INTO actors (AID, actor_name) VALUES (5, 'Charlie Chaplin');
 
@@ -19,3 +20,4 @@ INSERT INTO actor_rol (MID, AID, rol_name) VALUES (5, 3, 'Protagonist');
 INSERT INTO actor_rol (MID, AID, rol_name) VALUES (5, 1, 'Cameraman');
 INSERT INTO actor_rol (MID, AID, rol_name) VALUES (4, 4, 'Secondary character');
 INSERT INTO actor_rol (MID, AID, rol_name) VALUES (1, 5, 'Director');
+INSERT INTO actor_rol (MID, AID, rol_name) VALUES (0, 40, '');
