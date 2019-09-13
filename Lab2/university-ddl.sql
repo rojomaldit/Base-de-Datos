8d5a016d298af6f60e00053e584755e80a63fdaf
rojomaldit <lucaserfurt@hotmail.com>
2019-09-13 13:36:15 -0300

    Re-struct

--- Lab2/university-ddl.sql
+++ Lab2/university-ddl.sql
@@ -5,31 +5,4 @@ WITH aux AS (
 )
 SELECT MAX(ins), MIN(ins) from aux;
 
--- *********************** CARDELINO ***********************
 
--- DROP VIEW IF EXISTS aux;
--- 
--- CREATE VIEW aux AS
---   SELECT count(takes.ID) AS section_inscriptions from takes, section
---     WHERE
---       takes.course_id = section.course_id AND
---       takes.semester = section.semester AND
---       takes.year = section.year
---     GROUP BY
---       takes.course_id;
--- 
--- SELECT MAX(section_inscriptions), MIN(section_inscriptions) FROM aux;
-
--- ************************** OCTA **************************
-
--- WITH
---   aux AS (
---     SELECT sec_id, course_id, semester, year, count(takes.ID) AS inscriptions
---       from takes, section
---     GROUP BY
---       section.course_id,
---       section.semester,
---       section.sec_id,
---       section.year
---   )
--- SELECT MAX(inscriptions), MIN(inscriptions) from aux;
