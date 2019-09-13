d6dc270e08cca47155f8fcd43b3f3e3036105e15
octaviotastico <octaviopercivaldi2@gmail.com>
2019-09-13 16:36:53 -0300

    Finished EJ 3

---
+++ Lab2/insertxd.sql
@@ -0,0 +1,2 @@
+insert into section (course_id, sec_id, semester, year, building, room_number, time_slot_id)
+values ('101', '4', 'Fall', 2025, 'Alumni', '143', 'A')
--- Lab2/university-ddl.sql
+++ Lab2/university-ddl.sql
@@ -1,10 +1,11 @@
+-- EJ 1
 WITH aux AS (
-  SELECT count(takes.ID) AS ins FROM takes
+  SELECT count(ID) AS ins FROM takes
   GROUP BY course_id, semester, sec_id, year
 )
 SELECT MAX(ins), MIN(ins) FROM aux;
 
-
+-- EJ 2
 SELECT count(takes.ID) AS cnt, course_id, semester, sec_id, year FROM takes
 GROUP BY course_id, semester, sec_id, year
 HAVING cnt = (
@@ -14,5 +15,15 @@ HAVING cnt = (
 	) AS aux
 );
 
+-- EJ 3
+SELECT MAX(ins), MIN(ins) FROM (
+  SELECT count(ID) AS ins
+  FROM takes as T RIGHT JOIN section as S
+  ON T.course_id = S.course_id
+  AND T.sec_id = S.sec_id
+  AND T.semester = S.semester
+  AND T.year = S.year
+  GROUP BY S.course_id, S.semester, S.sec_id, S.year
+) AS foo;
 
-
+-- EJ 4
