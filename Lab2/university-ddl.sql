7164297ebe141b1e7c851f76d26aedf49ec838b3
rojomaldit <lucaserfurt@hotmail.com>
2019-09-13 15:19:48 -0300

    Ej2

--- Lab2/university-ddl.sql
+++ Lab2/university-ddl.sql
@@ -1,8 +1,18 @@
 WITH aux AS (
-  SELECT count(takes.ID) AS ins
-    from takes
+  SELECT count(takes.ID) AS ins FROM takes
   GROUP BY course_id, semester, sec_id, year
 )
-SELECT MAX(ins), MIN(ins) from aux;
+SELECT MAX(ins), MIN(ins) FROM aux;
+
+
+SELECT count(takes.ID) AS cnt, course_id, semester, sec_id, year FROM takes
+GROUP BY course_id, semester, sec_id, year
+HAVING cnt = (
+	SELECT MAX(ins) FROM (
+		SELECT count(takes.ID) AS ins FROM takes
+		GROUP BY course_id, semester, sec_id, year
+	) AS aux
+);
+
 
 
