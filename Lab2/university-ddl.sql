ad6bbc72608ae82e336eac3ad2694db08cfcef46
rojomaldit <lucaserfurt@hotmail.com>
2019-09-25 15:44:52 -0300

    Fixed minor errors

--- Lab2/university-ddl.sql
+++ Lab2/university-ddl.sql
@@ -32,44 +32,28 @@ SELECT MAX(ins), MIN(ins) FROM (
 
 -- EJ 4
 SELECT * FROM course
-where title like "Di%";
+where title like "Comp%";
 
 -- EJ 5
-WITH E_CT AS(
-	SELECT DISTINCT student.ID 
-    FROM student LEFT JOIN (
-		SELECT DISTINCT takes.course_id, ID FROM takes LEFT JOIN course
-		ON takes.course_id = course.course_id
-    ) as CT
-	ON student.ID = CT.ID
+WITH R AS (
+	SELECT ID from course JOIN teaches
+	ON course.course_id = teaches.course_id
+	WHERE course.title LIKE "Comp%"
 )
-SELECT instructor.ID, name, dept_name, salary FROM E_CT 
-JOIN instructor ON E_CT.ID = instructor.ID;
+SELECT * FROM instructor as i JOIN R
+ON i.ID = R.ID;
 
--- EJ 8
-WITH cal AS(
-	SELECT sum(credits) AS sum, ID
-	FROM (
-			SELECT tot_cred, course_id, student.ID FROM takes, student
-			where takes.ID = student.ID
-		 ) AS ST JOIN course ON ST.course_id = course.course_ID
-	GROUP BY ID
-)
-UPDATE student AS s
-SET ID = s.ID,
-	name = s.name,
-    dept_name = s.dept_name,
-    tot_cred = (SELECT sum FROM cal WHERE cal.ID = s.ID);
 
-/* PRINT PARA VER QUE EL EJERCICIO ANTERIOR ANDA
-WITH cal AS (
-	SELECT sum(credits) AS sum, ID
-	FROM (
-			SELECT tot_cred, course_id, student.ID FROM takes JOIN student
-			ON takes.ID = student.ID
-		 ) AS ST JOIN course ON ST.course_id = course.course_ID
-	GROUP BY ID
+  
+-- EJ 9
+WITH R AS (
+	SELECT i.ID, count(sec_id) as count FROM instructor as i JOIN teaches as t
+	ON i.ID = t.ID
+	GROUP BY i.ID
 )
-SELECT tot_cred, sum, student.ID AS sid FROM student, cal 
-WHERE cal.ID = student.ID
-*/
+UPDATE instructor as ins
+SET salary = 1000 * (SELECT count FROM R WHERE R.ID = ins.ID);
+    
+    
+    
+    
